94b78c6f60b2ff5679cd335528fb74ea8759579f
Yerkebulan Sharipov <94905139+Yorkie10@users.noreply.github.com>
2023-09-27 14:51:20 +0600

    minor fixes

---
+++ WordsApp/Navigation/NavigationBar.swift
@@ -0,0 +1,30 @@
+//
+//  NavigationBar.swift
+//  WordsApp
+//
+//  Created by Yerkebulan Sharipov on 27.09.2023.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var title = ""
+    var body: some View {
+        ZStack {
+            Color.clear
+                .background(.ultraThinMaterial)
+                .blur(radius: 10)
+            Text(title)
+                .font(.largeTitle.weight(.bold))
+                .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading, 20)
+        }
+            .frame(height: 70)
+            .frame(maxHeight: .infinity, alignment: .top)
+     
+    }
+}
+
+#Preview {
+    NavigationBar(title: "Featured")
+}
