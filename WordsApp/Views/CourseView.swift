b7c3f9330ebd573e2b7833ea19fe0e975b29486c
Yerkebulan Sharipov <94905139+Yorkie10@users.noreply.github.com>
2023-10-01 18:03:35 +0600

    minor fixes

---
+++ WordsApp/Views/CourseView.swift
@@ -0,0 +1,101 @@
+//
+//  CourseView.swift
+//  WordsApp
+//
+//  Created by Yerkebulan Sharipov on 01.10.2023.
+//
+
+import SwiftUI
+
+struct CourseView: View {
+    var namespace: Namespace.ID
+    @Binding var show: Bool
+    
+    var body: some View {
+        ZStack {
+            ScrollView {
+                 cover
+            }
+            .background(Color("Backround"))
+            .ignoresSafeArea()
+            Button {
+                withAnimation(.spring(response: 0.6, dampingFraction: 0.8)) {
+                    show.toggle()
+                }
+            } label: {
+                Image(systemName: "xmark")
+                    .font(.body.weight(.bold))
+                .foregroundColor(.secondary)
+                .padding(8)
+                .background(.ultraThinMaterial, in: Circle())
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+            .padding(20)
+            .ignoresSafeArea()
+        }
+    }
+    
+    var cover: some View {
+        VStack {
+            Spacer()
+        }
+        .frame(maxWidth: .infinity)
+
+        .frame(height: 500)
+        .foregroundStyle(.black)
+        .background(
+            Image("Illustration 9")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .matchedGeometryEffect(id: "image", in: namespace)
+        )
+        .background(
+            Image("Background 5")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .matchedGeometryEffect(id: "backround", in: namespace)
+        )
+        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous)
+        .matchedGeometryEffect(id: "mask", in: namespace))
+        .overlay(
+            VStack(alignment: .leading, spacing: 12)  {
+                Text("swiftUI")
+                    .font(.largeTitle.weight(.bold))
+                    .matchedGeometryEffect(id: "title", in: namespace)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text("20 sections - 3 hours".uppercased())
+                    .font(.footnote.weight(.semibold))
+                    .matchedGeometryEffect(id: "subtitle", in: namespace)
+                Text("Build an iOS app for iOS 15 with custom layouts, animations and...")
+                    .font(.footnote)
+                    .matchedGeometryEffect(id: "text", in: namespace)
+                Divider()
+                HStack {
+                    Image("Avatar Default")
+                        .resizable()
+                        .frame(width: 26, height: 26)
+                        .cornerRadius(10)
+                        .padding(8)
+                        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 18, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
+                        .strokeStyle(cornerRadius: 18)
+                    Text("Taught by York")
+                        .font(.footnote)
+                }
+            }
+                .padding(20)
+                .background(
+                Rectangle()
+                    .fill(.ultraThinMaterial)
+                .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                .matchedGeometryEffect(id: "blur", in: namespace)
+                )
+                .offset(y: 250)
+                .padding(20)
+        )
+    }
+}
+
+#Preview {
+    @Namespace static var namespace
+    CourseView(namespace: namespace, show: .constant(true))
+}
--- WordsApp/Views/HomeView.swift
+++ WordsApp/Views/HomeView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct HomeView: View {
     @State var hasScrolled = false
+    @Namespace var namespace
+    @State var show = false
     
     var body: some View {
         ZStack {
@@ -17,7 +19,19 @@ struct HomeView: View {
             ScrollView {
                 scrollDetection
                 featured
-                Color.clear.frame(height: 1000)
+                Text("Courses".uppercased())
+                    .font(.footnote.weight(.semibold))
+                    .foregroundColor(.secondary)
+                    .frame(minWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal, 20)
+                if !show {
+                    CourseItem(namespace: namespace, show: $show)
+                        .onTapGesture {
+                            withAnimation(.spring(response: 0.6, dampingFraction: 0.8)) {
+                                show.toggle()
+                            }
+                        }
+                }
             }
             .coordinateSpace(name: "scroll")
             .safeAreaInset(edge: .top, content: {
@@ -26,6 +40,9 @@ struct HomeView: View {
             .overlay(
                 NavigationBar(title: "Featured", hasScrolled: $hasScrolled)
             )
+            if show {
+                CourseView(namespace: namespace, show: $show)
+            }
         }
     }
     
